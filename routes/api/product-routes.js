cbeaa435b303daa25ea4e3214e7cfdb27e94c6ed
Davis Balan <balandavis6@gmail.com>
2024-05-04 16:03:44 -0500

    added to product tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,24 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      // JOIN with travellers, using the Trip through table
+      include: [{ model: Category, Tag }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // find one category by its `id` value
+  // be sure to include its associated Products
 });
 
 // create new product
@@ -100,8 +115,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No category with this id'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
